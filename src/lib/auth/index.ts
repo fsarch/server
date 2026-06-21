580b0436e210a8adc66ecfce465a340b78d4325e
Michael Braun <michael@web4brauns.de>
2026-06-21 20:15:17 +0200

    Add exports for module configuration and user data decorators

--- src/lib/auth/index.ts
+++ src/lib/auth/index.ts
@@ -3,5 +3,13 @@ export {
 } from './decorators/public.decorator.js';
 
 export {
+  UserData,
+} from './decorators/user-data.decorator.js';
+
+export {
   AuthGuard,
 } from './guards/auth.guard.js';
+
+export {
+  User,
+} from './user.js';
---
+++ src/lib/configuration/index.ts
@@ -0,0 +1,7 @@
+export {
+  ModuleConfigurationService,
+} from './module/module-configuration.service.js';
+
+export {
+  ModuleConfiguration,
+} from './module/module-configuration.module.js';
